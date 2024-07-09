b278033efff301822798b0aa9c3226a66d26a50b
Victor-OT <crowbreaker96@hotmail.com>
2024-07-08 21:24:33 -0600

    Se desarrolló todo el Header, incluyendo estilos

--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,9 +1,13 @@
+import './MainPage.css'
 function MainPage () {
     return (
         <header>
             <img src='../../assets/img/logo.svg' alt='logo' />
-            <p>La próxima revolución en el intercambio de criptomonedas.</p>         
-            <p>Batatabit te ayuda a navegar entre los diferentes precios y tendencias</p>         
+            <div className='header-text-container'>
+                <p className='header-title'>La próxima revolución en el intercambio de criptomonedas.</p>         
+                <p className='header-text'>Batatabit te ayuda a navegar entre los diferentes precios y tendencias.</p> 
+            </div>  
+            <button>Conoce Nuestros Planes <span><img src="../../assets/icons/down-arrow.svg" alt="" /></span></button>      
         </header>
     )
 
